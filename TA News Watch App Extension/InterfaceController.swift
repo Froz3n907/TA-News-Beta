1775b35a09b3867e2cf76aed75a4806f3b86165d
tobywoollaston <tobywoollaston@hotmail.co.uk>
2016-05-08 23:24:19 +0100

    apple watch

---
+++ TA News Watch App Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  TA News Watch App Extension
+//
+//  Created by Toby Woollaston on 08/05/2016.
+//  Copyright © 2016 FutureAppleCEO. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
